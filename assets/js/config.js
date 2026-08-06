f1ae88bac5bbefb924b64192cf637108c305b0c8
chairilanwar26 <contact.anandabintang@gmail.com>
2026-08-06 21:52:26 +0700

    delete

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.KARTOGRAFI_CONFIG = {
-  localCoverBase: "./assets/covers/",
-  productionCoverBase: "https://risethumdigsasindo.github.io/Kartografi-Sampul-Sastra-Indonesia/covers/",
-  portfolioBackUrl: "../",
-  dashboardUrl: "https://risethumdigsasindo.github.io/Kartografi-Sampul-Sastra-Indonesia/"
-};
